2544393810e34d62a9ade1e3b5276bcd23784536
Nathan Lynch <nathan_lynch@mentor.com>
2014-03-26 12:55:53 -0500

    add gettimeofday test suite

---
+++ src/gettimeofday.c
@@ -0,0 +1,145 @@
+#include <errno.h>
+#include <error.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include "compiler.h"
+#include "vdsotest.h"
+
+#define USEC_PER_SEC 1000000
+
+static void gettimeofday_syscall_nofail(struct timeval *tv)
+{
+	int err;
+
+	err = syscall(SYS_gettimeofday, tv, NULL);
+	if (err)
+		error(EXIT_FAILURE, errno, "SYS_gettimeofday");
+}
+
+static void gettimeofday_libc_nofail(struct timeval *tv)
+{
+	int err;
+
+	err = gettimeofday(tv, NULL);
+	if (err)
+		error(EXIT_FAILURE, errno, "gettimeofday");
+}
+
+static bool timevals_ordered(const struct timeval *first,
+			     const struct timeval *second)
+{
+	if (first->tv_sec < second->tv_sec)
+		return true;
+
+	if (first->tv_sec == second->tv_sec)
+		return first->tv_usec <= second->tv_usec;
+
+	return false;
+}
+
+static bool timeval_normalized(const struct timeval *tv)
+{
+	if (tv->tv_sec < 0)
+		return false;
+	if (tv->tv_usec < 0)
+		return false;
+	if (tv->tv_usec >= USEC_PER_SEC)
+		return false;
+	return true;
+}
+
+static void gettimeofday_verify(struct ctx *ctx)
+{
+	struct timeval now;
+
+	gettimeofday_syscall_nofail(&now);
+
+	ctx_start_timer(ctx);
+
+	while (!test_should_stop(ctx)) {
+		struct timeval prev;
+
+		prev = now;
+
+		gettimeofday_libc_nofail(&now);
+
+		if (!timeval_normalized(&now)) {
+			log_failure(ctx, "timestamp obtained from libc/vDSO "
+				    "not normalized:\n"
+				    "\t[%ld, %ld]\n",
+				    now.tv_sec, now.tv_usec);
+		}
+
+		if (!timevals_ordered(&prev, &now)) {
+			log_failure(ctx, "timestamp obtained from libc/vDSO "
+				    "predates timestamp\n"
+				    "previously obtained from kernel:\n"
+				    "\t[%ld, %ld] (kernel)\n"
+				    "\t[%ld, %ld] (vDSO)\n",
+				    prev.tv_sec, prev.tv_usec,
+				    now.tv_sec, now.tv_usec);
+		}
+
+		prev = now;
+
+		gettimeofday_syscall_nofail(&now);
+
+		if (!timeval_normalized(&now)) {
+			log_failure(ctx, "timestamp obtained from kernel "
+				    "not normalized:\n"
+				    "\t[%ld, %ld]\n",
+				    now.tv_sec, now.tv_usec);
+		}
+
+		if (!timevals_ordered(&prev, &now)) {
+			log_failure(ctx, "timestamp obtained from kernel "
+				    "predates timestamp\n"
+				    "previously obtained from libc/vDSO:\n"
+				    "\t[%ld, %ld] (vDSO)\n"
+				    "\t[%ld, %ld] (kernel)\n",
+				    prev.tv_sec, prev.tv_usec,
+				    now.tv_sec, now.tv_usec);
+		}
+
+	}
+}
+
+static void gettimeofday_bench(struct ctx *ctx, struct bench_results *res)
+{
+	uint64_t vdsocalls;
+	uint64_t syscalls;
+	struct timeval tv;
+
+	ctx_start_timer(ctx);
+
+	for (vdsocalls = 0; !test_should_stop(ctx); vdsocalls++) {
+		gettimeofday(&tv, NULL);
+	}
+
+	ctx_start_timer(ctx);
+
+	for (syscalls = 0; !test_should_stop(ctx); syscalls++) {
+		syscall(SYS_gettimeofday, &tv, NULL);
+	}
+
+	printf("%s: syscalls = %llu, vdso calls = %llu\n", __func__,
+	       (unsigned long long)syscalls, (unsigned long long)vdsocalls);
+}
+
+
+static const struct test_suite gettimeofday_ts = {
+	.name = "gettimeofday",
+	.bench = gettimeofday_bench,
+	.verify = gettimeofday_verify,
+};
+
+static void __constructor gettimeofday_init(void)
+{
+	register_testsuite(&gettimeofday_ts);
+}
