6f3d6655e23fac69e88bc5bd3e40f16027486475
Nathan Lynch <nathan_lynch@mentor.com>
2014-03-26 11:49:02 -0500

    add __printf function attribute

--- src/compiler.h
+++ src/compiler.h
@@ -1,6 +1,7 @@
 #ifndef VDSOTEST_COMPILER_H
 #define VDSOTEST_COMPILER_H
 
-#define __constructor __attribute__((constructor))
+#define __constructor  __attribute__((constructor))
+#define __printf(a, b) __attribute__((format(printf, a, b)))
 
 #endif
