c4fa3902d2948ec9c40a3ce5a838bac19f6fa884
Nathan Lynch <nathan_lynch@mentor.com>
2014-03-26 10:16:15 -0500

    add hashtable helper APIs

--- src/util.c
+++ src/util.c
@@ -1,6 +1,11 @@
+#include <assert.h>
+#include <search.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "util.h"
+
+#define HASHTABLE_SZ 32
 
 void *xmalloc(size_t sz)
 {
@@ -32,4 +37,51 @@ void *xrealloc(void *ptr, size_t sz)
 void xfree(void *ptr)
 {
 	free(ptr);
+}
+
+static void hashtable_init(struct hashtable *ht)
+{
+	struct hsearch_data *htab;
+
+	assert(!ht->htab);
+
+	htab = xzmalloc(sizeof(*htab));
+
+	if (!hcreate_r(HASHTABLE_SZ, htab))
+		abort();
+
+	ht->htab = htab;
+}
+
+void *hashtable_lookup(struct hashtable *ht, const char *key)
+{
+	ENTRY search;
+	ENTRY *res;
+
+	if (!ht->htab)
+		hashtable_init(ht);
+
+	search = (ENTRY) {
+		.key = (void *)key,
+	};
+
+	hsearch_r(search, FIND, &res, ht->htab);
+
+	return res ? res->data : NULL;
+}
+
+void hashtable_add(struct hashtable *ht, const char *key, const void *data)
+{
+	ENTRY search;
+	ENTRY *res;
+
+	assert(!hashtable_lookup(ht, key));
+
+	search = (ENTRY) {
+		.key = (void *)key,
+		.data = (void *)data,
+	};
+
+	if (!hsearch_r(search, ENTER, &res, ht->htab))
+		abort();
 }
--- src/util.h
+++ src/util.h
@@ -1,10 +1,20 @@
 #ifndef VDSOTEST_UTIL_H
 #define VDSOTEST_UTIL_H
 
+#include <search.h>
+
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 void *xmalloc(size_t sz);
 void *xzmalloc(size_t sz);
 void *xrealloc(void *ptr, size_t sz);
 void xfree(void *ptr);
+
+struct hashtable {
+	struct hsearch_data *htab;
+};
+
+void *hashtable_lookup(struct hashtable *ht, const char *key);
+void hashtable_add(struct hashtable *ht, const char *key, const void *data);
+
 #endif
