36d162d7709afea232744df39b8c922e46ad1e68
Nathan Lynch <nathan_lynch@mentor.com>
2014-03-31 10:55:55 -0500

    gettimeofday: rework ABI tests
    Let's "generate" the tests at runtime so we can cover all
    permutations of values for tv and tz.

--- src/gettimeofday.c
+++ src/gettimeofday.c
@@ -1,9 +1,11 @@
+#include <assert.h>
 #include <errno.h>
 #include <error.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
 #include <sys/syscall.h>
 #include <sys/time.h>
 #include <unistd.h>
@@ -141,77 +143,200 @@ static void gettimeofday_bench(struct ctx *ctx, struct bench_results *res)
 struct gettimeofday_args {
 	struct timeval *tv;
 	struct timezone *tz;
+	bool force_syscall;
 };
 
-struct timezone gtod_args_tz;
-struct timeval gtod_args_tv;
+enum gtod_arg_type {
+	valid,
+	nullptr,
+	bogus,
+	prot_none,
+	prot_read,
+	gtod_arg_type_max,
+};
 
-static struct gettimeofday_args both_null;
-static struct gettimeofday_args tz_null = { .tv = &gtod_args_tv, };
-static struct gettimeofday_args tv_null = { .tz = &gtod_args_tz, };
+static const char *gtod_arg_type_str[] = {
+	[valid] = "valid",
+	[nullptr] = "NULL",
+	[bogus] = "UINTPTR_MAX",
+	[prot_none] = "page (PROT_NONE)",
+	[prot_read] = "page (PROT_READ)",
+};
 
-static void sys_gettimeofday_simple(void *arg, struct syscall_result *res)
+static void do_gettimeofday(void *arg, struct syscall_result *res)
 {
 	struct gettimeofday_args *args = arg;
 	int err;
 
 	syscall_prepare();
-	err = syscall(SYS_gettimeofday, args->tv, args->tz);
+	if (args->force_syscall)
+		err = syscall(SYS_gettimeofday, args->tv, args->tz);
+	else
+		err = gettimeofday(args->tv, args->tz);
 	record_syscall_result(res, err, errno);
 }
 
-static void gettimeofday_simple(void *arg, struct syscall_result *res)
+static void *gtod_arg_alloc(enum gtod_arg_type t)
 {
-	struct gettimeofday_args *args = arg;
-	int err;
+	void *ret;
 
-	syscall_prepare();
-	err = gettimeofday(args->tv, args->tz);
-	record_syscall_result(res, err, errno);
+	switch (t) {
+	case valid:
+		ret = xmalloc(sysconf(_SC_PAGESIZE));
+		break;
+	case nullptr:
+		ret = NULL;
+		break;
+	case bogus:
+		ret = (void *)ADDR_SPACE_END;
+		break;
+	case prot_none:
+		ret = alloc_page(PROT_NONE);
+		break;
+	case prot_read:
+		ret = alloc_page(PROT_READ);
+		break;
+	default:
+		assert(false);
+		break;
+	}
+
+	return ret;
 }
 
-static const struct child_params gettimeofday_abi_params[] = {
-	/* Kernel sanity checks */
-	{
-		.desc = "SYS_gettimeofday(NULL, NULL)",
-		.func = sys_gettimeofday_simple,
-		.arg = &both_null,
-	},
-	{
-		.desc = "SYS_gettimeofday(tv, NULL)",
-		.func = sys_gettimeofday_simple,
-		.arg = &tz_null,
-	},
-	{
-		.desc = "SYS_gettimeofday(NULL, tz)",
-		.func = sys_gettimeofday_simple,
-		.arg = &tv_null,
-	},
+static void gtod_arg_release(void *buf, enum gtod_arg_type t)
+{
+	switch (t) {
+	case valid:
+		xfree(buf);
+		break;
+	case nullptr:
+	case bogus:
+		break;
+	case prot_none:
+	case prot_read:
+		free_page(buf);
+		break;
+	default:
+		assert(false);
+		break;
+	}
+}
 
-	/* The below may be serviced by a vDSO, but not necessarily. */
-	{
-		.desc = "gettimeofday(NULL, NULL)",
-		.func = gettimeofday_simple,
-		.arg = &both_null,
-	},
-	{
-		.desc = "gettimeofday(tv, NULL)",
-		.func = gettimeofday_simple,
-		.arg = &tz_null,
-	},
-	{
-		.desc = "gettimeofday(NULL, tz)",
-		.func = gettimeofday_simple,
-		.arg = &tv_null,
-	},
-};
+static bool __pure gtod_args_should_fault(enum gtod_arg_type tv,
+					  enum gtod_arg_type tz)
+{
+	switch (tv) {
+	case valid:
+	case nullptr:
+		break;
+	case bogus:
+	case prot_none:
+	case prot_read:
+		return true;
+		break;
+	default:
+		assert(false);
+		break;
+	}
+
+	switch (tz) {
+	case valid:
+	case nullptr:
+		break;
+	case bogus:
+	case prot_none:
+	case prot_read:
+		return true;
+		break;
+	default:
+		assert(false);
+		break;
+	}
+
+	return false;
+}
 
 static void gettimeofday_abi(struct ctx *ctx)
 {
-	unsigned int i;
+	enum gtod_arg_type tv_type;
 
-	for (i = 0; i < ARRAY_SIZE(gettimeofday_abi_params); i++)
-		run_as_child(ctx, &gettimeofday_abi_params[i]);
+	for (tv_type = 0; tv_type < gtod_arg_type_max; tv_type++) {
+		enum gtod_arg_type tz_type;
+		struct timeval *tv;
+
+		tv = gtod_arg_alloc(tv_type);
+
+		for (tz_type = 0; tz_type < gtod_arg_type_max; tz_type++) {
+			struct gettimeofday_args args;
+			struct signal_set signal_set;
+			struct child_params parms;
+			struct timezone *tz;
+			int expected_errno;
+			int expected_ret;
+			char *desc;
+
+			tz = gtod_arg_alloc(tz_type);
+
+			/* First, force system call */
+			args = (struct gettimeofday_args) {
+				.tv = tv,
+				.tz = tz,
+				.force_syscall = true,
+			};
+
+			expected_ret = 0;
+			if (gtod_args_should_fault(tv_type, tz_type))
+				expected_ret = -1;
+
+			expected_errno = 0;
+			if (gtod_args_should_fault(tv_type, tz_type))
+				expected_errno = EFAULT;
+
+			/* Should never actually terminate by signal
+			 * for syscall.
+			 */
+			signal_set.mask = 0;
+
+			xasprintf(&desc, "SYS_gettimeofday(%s, %s)",
+				  gtod_arg_type_str[tv_type],
+				  gtod_arg_type_str[tz_type]);
+
+			parms = (struct child_params) {
+				.desc = desc,
+				.func = do_gettimeofday,
+				.arg = &args,
+				.expected_ret = expected_ret,
+				.expected_errno = expected_errno,
+				.signal_set = signal_set,
+			};
+
+			run_as_child(ctx, &parms);
+
+			xfree(desc);
+
+			/* Now do libc/vDSO */
+
+			args.force_syscall = false;
+			if (gtod_args_should_fault(tv_type, tz_type))
+				signal_set.mask |= SIGNO_TO_BIT(SIGSEGV);
+
+			xasprintf(&desc, "gettimeofday(%s, %s)",
+				  gtod_arg_type_str[tv_type],
+				  gtod_arg_type_str[tz_type]);
+
+			parms.desc = desc;
+			parms.signal_set = signal_set;
+
+			run_as_child(ctx, &parms);
+
+			xfree(desc);
+
+			gtod_arg_release(tz, tz_type);
+		}
+
+		gtod_arg_release(tv, tv_type);
+	}
 }
 
 static const struct test_suite gettimeofday_ts = {
