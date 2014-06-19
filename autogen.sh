dd2ad629bf0f3d6dbcfba493f18e70c56def8257
Nathan Lynch <nathan_lynch@mentor.com>
2014-06-19 15:47:37 -0500

    autogen.sh: remove verbose stuff

--- autogen.sh
+++ autogen.sh
@@ -7,19 +7,3 @@ if [ -f .git/hooks/pre-commit.sample -a ! -f .git/hooks/pre-commit ] ; then
 fi
 
 autoreconf --install --symlink
-
-libdir() {
-        echo $(cd $1/$(gcc -print-multi-os-directory); pwd)
-}
-
-args="--prefix=/usr \
---sysconfdir=/etc \
---libdir=$(libdir /usr/lib)"
-
-echo
-echo "----------------------------------------------------------------"
-echo "Initialized build system. For a common configuration please run:"
-echo "----------------------------------------------------------------"
-echo
-echo "./configure CFLAGS='-g -O2' $args"
-echo
